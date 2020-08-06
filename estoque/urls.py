9d3d25653854879674641dcff1713ca330bf2707
Nathan Bahia <nathanbabahia@gmail.com>
2020-08-06 16:12:41 -0300

    Verifica o formulário antes da submissão e modifica arquivo estoqueJSON

--- estoque/urls.py
+++ estoque/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from .views import IndexView
 
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+
+    path('', login_required(IndexView.as_view()), name='index')
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import FormView
 from .forms import ProdutoModelForm
 from .models import ProdutoModel
+from django.contrib import messages
 
 
 class IndexView(FormView):
@@ -9,12 +10,13 @@ class IndexView(FormView):
     form_class = ProdutoModelForm
     success_url = reverse_lazy("index")
 
+
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         return context
 
-    def form_valid(self, form):
 
+    def form_valid(self, form):
         cod = form.cleaned_data['codigo']
         produto = ProdutoModel.objects.filter(codigo=cod).first()
         if produto is None:
@@ -23,10 +25,10 @@ class IndexView(FormView):
             quantidade = form.cleaned_data['contagem']
             produto.contagem += float(quantidade)
             produto.save()
-
-            print(float(quantidade))
-
+            messages.success(self.request, f'Produto atuzalizado com sucesso:\n{produto.nome} ')
         return super(IndexView, self).form_valid(form)
 
+
     def form_invalid(self, form):
+        messages.error(self.request, 'Erro ao atualizar produto.')
         return super(IndexView, self).form_invalid(form)
